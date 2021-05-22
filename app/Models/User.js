b0aec9764028c8c84bf748097886e48cb65e7bc5
Caroline Martins <53910116+CarolineBarbosaM@users.noreply.github.com>
2021-05-22 10:30:12 -0300

    model usuários

--- app/Models/User.js
+++ app/Models/User.js
@@ -8,12 +8,8 @@ const Hash = use('Hash')
 
 class User extends Model {
   static boot () {
-    super.boot()
 
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
+    super.boot()
     this.addHook('beforeSave', async (userInstance) => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
@@ -21,19 +17,6 @@ class User extends Model {
     })
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
-  tokens () {
-    return this.hasMany('App/Models/Token')
-  }
 }
 
 module.exports = User
