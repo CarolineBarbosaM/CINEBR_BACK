caad884c596b8e6670ed1d2e867e557e2642f846
CindySalvado <cindylsalvado@icloud.com>
2021-05-23 16:26:16 -0300

    Models de Serie e Entreterimento

--- app/Models/Series.js
+++ app/Models/Series.js
@@ -4,6 +4,30 @@
 const Model = use('Model')
 
 class Series extends Model {
-}
+    static boot () {
+        super.boot();
+        this.addHook('beforeSave', 'SeriesHook.uuid');
+    }
 
+    static get table () {
+    return 'series'
+  }
+
+  static get primaryKey(){
+    return 'id';
+  }
+
+  static get incrementing(){
+    return false;
+  }
+
+  ator() {
+    return this.hasMany('App/Models/Ator')
+  }
+
+  categoria() {
+    return this.belongsToMany('App/Models/Categoria')
+  }
+
+}
 module.exports = Series
