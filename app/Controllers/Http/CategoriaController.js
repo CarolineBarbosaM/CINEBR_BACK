3122d11c729cdd7e1abea6548f3135795a459ef4
robsalves <roberta_alves2018@hotmail.com>
2021-05-22 21:29:36 -0300

    Iniciando minha controller

---
+++ app/Controllers/Http/CategoriaController.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class CategoriaController {
+    async create ({ request, response, auth }) {
+   
+    }
+  
+    async list ({ request, response }) {
+    }
+  
+  
+    async listAll ({ params, request, response, view }) {
+    }
+  
+  
+    async update ({ params, request, response }) {
+    }
+  
+  
+    async delete ({ params, request, response }) {
+      
+  
+  
+    }
+}
+
+module.exports = CategoriaController
