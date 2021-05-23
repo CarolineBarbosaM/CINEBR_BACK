651dfffee5b1c5e4e7e8bd9c971d5dcc7c7fc525
robsalves <roberta_alves2018@hotmail.com>
2021-05-23 18:11:24 -0300

    Teste Rotas

--- start/routes.js
+++ start/routes.js
@@ -16,6 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.get('/', () => {
-  return { greeting: 'Hello world in JSON' }
-})
+{
+Route.resource('categoria', 'CategoriaController').
+apiOnly()
+
+}
