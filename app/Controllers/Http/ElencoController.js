f85de2a01abeef69c5714b5830077021b07b6009
Caroline Martins <53910116+CarolineBarbosaM@users.noreply.github.com>
2021-06-01 01:16:47 -0300

    Ajustes Elenco

--- app/Controllers/Http/ElencoController.js
+++ app/Controllers/Http/ElencoController.js
@@ -1,6 +1,7 @@
 'use strict'
 const Elenco= use('App/Models/Elenco')
 const Database = use('Database');
+const moment = require('moment');
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -10,84 +11,68 @@ const Database = use('Database');
  * Resourceful controller for interacting with elencos
  */
 class ElencoController {
-  async create ({ request, response, auth }) {
+  async create ({ request, response }) {
     try {
       const elenco = request.only(["id_entreterimento","atores"])
 
-      await elenco.create(elenco);
+      await Elenco.create(elenco);
 
       return response.status(200).json({ "message": 'Elenco cadastrada com sucesso.' });
-
-        } 
-   
-        catch(e) {
-      return response.status(500).json({"message": 'Erro ao cadastrar elenco'}); 
+    }
+    catch(e) {
+      return response.status(500).json({"message": 'Erro ao cadastrar elenco'});
     }
 
   }
 
   async list ({ request, response }) {
-  try {
-    const { id } = request.params;
-    const user = await Database.from("elenco").where('id', id)
+    try {
+      const { id } = request.params;
+      const elenco = await Database.from("elencos").where('id', id)
 
-    if (elenco == ''){
-
-      return response.status(200).json({"message": "message":"Elenco não foi encontrado" })
-                      }
-      return response.status(200).json({ elenco });
-     } catch(e){
-
-        return response.status(500).json({ "message": "Erro ao listar elenco" });
-                }
-                                    }
-   
- 
+      if (elenco == ''){
+        return response.status(200).json({"message": "Elenco não foi encontrado" })
+      }
+      return response.status(200).json(elenco);
+    } catch(e){
+      return response.status(500).json({ "message": "Erro ao listar elenco" });
+    }
+  }
 
   async listAll ({ response }) {
-    try { 
+    try {
       const elenco = await Database
-        .from('elenco')
+        .from('elencos')
         .where('deleted_at',null)
         .select('*')
 
-        return response.status(200).json({ elenco });
-
-       } 
- 
-        catch(e) {
-            return response.status(500).json({ "message":"Erro ao listar todos os elencos"})
-
-                }     
-                            }     
-
+        return response.status(200).json(elenco);
+    }
+    catch(e) {
+      return response.status(500).json({ "message":"Erro ao listar todos os elencos"})
+    }
+  }
 
   async update ({ request, response }) {
-      try{
-          const { id } = request.params;
-          const { atores, id_entreterimento } = request.body
+    try{
+      const { id } = request.params;
+      const { id_entreterimento, atores } = request.body
 
-          await elenco.query()
-          .from('elenco')
-          .where('id', id)
-          .update({
-            
-            id_entreterimento,
-            atores,
-            updated_at: moment().format("YYYY-MM-DD HH:mm:ss")
-            
-                  })
+      await Elenco.query()
+      .from('elencos')
+      .where('id', id)
+      .update({
+        id_entreterimento,
+        atores,
+        updated_at: moment().format("YYYY-MM-DD HH:mm:ss")
+      })
 
-            return response.status(200).json({ "message": "Elenco atualizado com sucesso." })
-
-        } 
-      
-      catch (e){
-        
-        return response.status(500).json({ "mensage": "Erro ao atualizado filmes." });
-
-              }
-                                  }
+      return response.status(200).json({ "message": "Elenco atualizado com sucesso." })
+    }
+    catch (e){
+      return response.status(500).json({ "mensage": "Erro ao atualizado elenco.", e });
+    }
+  }
 
 
   async delete ({ request, response }) {
@@ -95,8 +80,8 @@ class ElencoController {
         try {
             const { id }= request.params;
 
-            await elenco.query()
-            .from('elenco')
+            await Elenco.query()
+            .from('elencos')
             .where('id', id)
             .update({
               deleted_at: moment().format("YYYY-MM-DD HH:mm:ss")
